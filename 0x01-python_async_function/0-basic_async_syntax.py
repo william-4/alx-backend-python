fbf4fb0cbe63219002b2629797ba0bf6aef16fd9
william-4 <mwangiiwilliam@gmail.com>
2023-01-10 05:55:29 +0300

    rm trailing whitespace

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -10,4 +10,4 @@ async def wait_random(max_delay: int = 10) -> float:
     """
     secs = random.uniform(0, max_delay)
     await asyncio.sleep(secs)
-    return 
+    return
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -16,4 +16,4 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     for i in range(n):
         task = await wait_random(max_delay)
         delays.append(task)
-    return 
+    return
