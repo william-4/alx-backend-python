00daae1fc167ca60d0cede56f999f6d37dc63b04
william-4 <mwangiiwilliam@gmail.com>
2023-01-31 05:34:09 +0300

    task 1, 2, 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,9 @@ Test module for utils
 """
 
 import unittest
-from utils import access_nested_map
+import requests
+from unittest.mock import patch
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized, parameterized_class
 from typing import Mapping, Sequence, Any
 
@@ -20,10 +22,64 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map: Mapping,
-                               path: Sequence, expected: int) -> None:
+                               path: Sequence, expected) -> None:
         """
         Method to test that the method returns
         what is is supposed to
         """
         value = access_nested_map(nested_map, path)
         self.assertEqual(value, expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                                         path: Sequence) -> None:
+        """
+        Test that a KeyError is raised
+        """
+        with self.assertRaises(Exception):
+            access_nested_map(nested_map, path)
+
+class TestGetJson(unittest.TestCase):
+    """
+    Class to implement tests for the get_json function
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch("requests.get")
+    def test_get_json(self, test_url, test_payload, mock_requests_get):
+        """
+        Test the get_json method to ensure it returns the expected output.
+        """
+        mock_requests_get.return_value.json.return_value = test_payload
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
+        mock_requests_get.assert_called_once_with(test_url)
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test the memoization decorator, memoize
+    """
+    def test_memoize(self):
+        """
+        Test that utils.memoize decorator works as intended
+        """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, 'a_method') as mock_object:
+            test = TestClass()
+            test.a_property()
+            test.a_property()
+            mock_object.assert_called_once()
+
+
